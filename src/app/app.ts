52c119f2b4bedb45ff34e16e0aa770adc64340b7
adam <adamnatanpassaretti@gmail.com>
2026-02-11 03:05:03 +0100

    added skill section icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
-import { ProjectCard } from './components/project-card/project-card';
+// import { ProjectCard } from './components/project-card/project-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ProjectCard],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
--- src/app/pages/homepage/homepage.ts
+++ src/app/pages/homepage/homepage.ts
@@ -3,6 +3,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';  
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';  
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faCss3Alt } from '@fortawesome/free-brands-svg-icons';
+import { faJs } from '@fortawesome/free-brands-svg-icons';
+import { faPhp } from '@fortawesome/free-brands-svg-icons';
+// import { faMySql } from '@fortawesome/free-solid-svg-icons';
 import { Project } from '../../models/project/project.model';
 import { ProjectCard } from '../../components/project-card/project-card';
 import { NgFor } from '@angular/common';
@@ -18,6 +23,11 @@ export class Homepage
   faGithub = faGithub;
   faLinkedin = faLinkedin;
   faInstagram = faInstagram;
+  faHtml5 = faHtml5;
+  faCss3Alt = faCss3Alt;
+  faJs = faJs;
+  faPhp = faPhp;
+  // faMySql = faMySql;
 
   projects: Project[] = [
     { id:1, title:'My App', description:'A short summary', imageUrl:'./test.png', link:'https://...' },
